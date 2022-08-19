788466909c6d9b34770a8594e41dfb6657130010
hp-pc <navyashree811@gmail.com>
2022-08-19 19:44:20 +0530

    [Navaya]Added : Create LinkedList add 30 and 56 to 70

--- src/com/bridgelabz/datastructure/LinkListMain.java
+++ src/com/bridgelabz/datastructure/LinkListMain.java
@@ -1,5 +1,5 @@
 /*
- * UC 1 : Lets create a simple Linked List of 56, 30, and 70.
+ * UC 2 : Ability to create Linked List by adding 30 and 56 to 70.
  */
 package com.bridgelabz.datastructure;
 
@@ -7,9 +7,9 @@ public class LinkListMain {
 	public static void main(String[] args) {
 
 		Linklist<Integer> list1 = new Linklist();
-		list1.push(70);
-		list1.push(30);
-		list1.push(56);
+		list1.add(70);
+		list1.add(30);
+		list1.add(56);
 		list1.show();
 
 		System.out.println();
--- src/com/bridgelabz/datastructure/Linklist.java
+++ src/com/bridgelabz/datastructure/Linklist.java
@@ -4,14 +4,14 @@ public class Linklist<T> {
 	Node<T> head;
 	Node<T> tail;
 
-	public void push(T data) {
+	public void add(T data) {
 		Node<T> newNode = new Node(data);
 		if (head == null) {
 			head = newNode;
 			tail = newNode;
 		} else {
-			newNode.next = head;
-			head = newNode;
+			tail.next = newNode;
+			tail = newNode;
 		}
 	}
 
