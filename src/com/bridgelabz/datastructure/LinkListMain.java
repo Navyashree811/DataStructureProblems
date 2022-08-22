d150b8538c6fd1cab20afca2d9cf0b9de5e3be5b
hp-pc <navyashree811@gmail.com>
2022-08-22 18:56:27 +0530

    Added : Delete 40 from the linkedlist

--- src/com/bridgelabz/datastructure/LinkListMain.java
+++ src/com/bridgelabz/datastructure/LinkListMain.java
@@ -1,5 +1,5 @@
 /*
- * UC 8 : Ability to insert 40 after 30 to the Linked List sequence of 56 -> 30-> 70.
+ * UC 9 : Ability to delete 40 from the LinkedList sequence of 56 -> 30 -> 40 -> 70.
  */
 package com.bridgelabz.datastructure;
 
@@ -10,9 +10,15 @@ public class LinkListMain {
 		list1.add(56);
 		list1.add(30);
 		list1.add(70);
+		
 		list1.insertAtMid(40);
+		System.out.println("List befor deletion");
+		list1.show();
+		list1.popDeleteData(2);
+		System.out.println("List after deletion");
 		list1.show();
 
+		System.out.println("The size of list is : " + list1.size());
 		System.out.println();
 
 	}
